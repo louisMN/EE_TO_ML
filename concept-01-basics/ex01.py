f7ac2fafea0f1cfa7af40f715b1b07df7048d17c
Martins <133435723+louisMN@users.noreply.github.com>
2026-09-16 18:55:16 +0200

    /concept-01-basics/ex01.py

---
+++ concept-01-basics/ex01.py
@@ -0,0 +1,5 @@
+# Task: Print exactly two lines: Hello, World! then My name is <your real name>.
+# Solution
+
+print("Hello, World!") # shows Hello, World! on the console.
+print("My name is James") # prints out the argument passed in the print() function
