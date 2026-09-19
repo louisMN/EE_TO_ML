94f86cfa44c96b69e28eb7d41b30570378ec6553
Martins <133435723+louisMN@users.noreply.github.com>
2026-09-19 11:46:33 +0200

    Done with ex07.py

---
+++ concept-01-basics/ex07.py
@@ -0,0 +1,14 @@
+# Task: Energy bill: ask for an appliance's power (watts), hours used per day, and tariff (currency per kWh). Print the daily cost and 30-day cost, 2 decimals. (Reminder: kWh = watts × hours ÷ 1000.)
+
+fixed_tariff = 0.05 # Initialize and fixes the tariff
+
+# Collects the appliance and power rating information from user
+appliance = input("What appliance would you like to check: ")
+rating = float(input("What is the power rating(watt) of the appliance:"))
+
+# Performs arithemetic calculation for the daily cost and the cost after 30 days
+daily_cost = (rating/1000) * 24 * fixed_tariff
+thirty_day_cost = daily_cost * 30
+
+# Displays the result
+print(f"The cost of running the {appliance.title()} is ${daily_cost:.2f} daily and ${thirty_day_cost:.2f} after thirty days")
