e16593a815c1a59e30dd8e0f13fb9ca94377d57b
Martins <133435723+louisMN@users.noreply.github.com>
2026-09-19 10:20:30 +0200

    Done with ex03.py

--- concept-01-basics/ex03.py
+++ concept-01-basics/ex03.py
@@ -1,2 +1,4 @@
+# Task: Ask the user's name with input(), then print Hello, <name>! Welcome to Python. using an f-string.
+
 name = input("What is your name: ") #Initializes the variable by asking for an input
 print(f"Hello, {name.title()}! Welcome to Python") #Displays the output to the console
