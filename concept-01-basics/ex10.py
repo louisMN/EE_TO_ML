c059efbdd5c154ef1cbb5c23be31acfc8389a654
Martins <133435723+louisMN@users.noreply.github.com>
2026-09-19 16:15:03 +0200

    feat(ex10): battery runtime estimator

---
+++ concept-01-basics/ex10.py
@@ -0,0 +1,11 @@
+# Task: Battery runtime estimator: ask for battery capacity (mAh) and device draw (mA). Print runtime formatted as 37h 30m (use // and %). Then print True if runtime ≥ 24 hours else False — without using if (a comparison like total_hours >= 24 already produces a bool).
+
+battery_capacity = int(input("Battery capacity (mAh): "))
+device_draw = int(input("Device draw: "))
+
+hours = battery_capacity // device_draw
+minutes = (((battery_capacity % device_draw) * 60)// device_draw) * 60
+
+print(f"Run time: {hours}h {minutes}m ")
+
+print(hours>= 24)
