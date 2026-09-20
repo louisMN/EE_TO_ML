bd5717c1af3116441146adc419fdfc4f4db9cf11
Martins <133435723+louisMN@users.noreply.github.com>
2026-09-20 07:12:56 +0200

    feat(int02): Flash Memory Pager

--- concept-01-basics/integration-test/int02.py
+++ concept-01-basics/integration-test/int02.py
@@ -5,10 +5,13 @@
     Print how many bytes will be written to the final, incomplete page.
     Format exactly as: Pages: X, Leftover bytes: Y. """
 
-capacity = 4096
-pages = int(input("Total number of bytes to be written: "))
 
-full_pages = pages // 4096
-left_over = (pages % 4096)
+capacity = 4096 #Initializing Microcontroller write capacity
+total_byte = int(input("Total number of bytes to be written: ")) #Collect user info on how many byte need to be written
 
+# Arithemetic operation to determine number of Full pages and left over bytes by the microcontroller
+full_pages = total_byte // 4096
+left_over = (total_byte % 4096)
+
+# Displays the Output
 print(f"Pages: {full_pages}, Leftover bytes: {left_over}")
